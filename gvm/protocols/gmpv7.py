9949a673a31af4b9cb5b222e88b54424b61ec611
Björn Ricks <bjoern.ricks@greenbone.net>
2018-11-07 16:35:52 +0100

    Ignore too many line pylint setting for gmpv7 module
    Currently with this low level api the file will grow even more. So it
    doesn't make sense to warn for to many lines.

--- gvm/protocols/gmpv7.py
+++ gvm/protocols/gmpv7.py
@@ -15,6 +15,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=too-many-lines
 """
 Module for communication with gvmd in Greenbone Management Protocol version 7
 """
