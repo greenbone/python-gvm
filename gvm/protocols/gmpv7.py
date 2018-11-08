ed8e6f8019200a485d0704ee46cec8cae562f862
Björn Ricks <bjoern.ricks@greenbone.net>
2018-11-08 12:01:44 +0100

    Ignore pylint warning for overriden builtins
    In this case filter and format are used as parameters for get methods.

--- gvm/protocols/gmpv7.py
+++ gvm/protocols/gmpv7.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=too-many-lines
+# pylint: disable=too-many-lines,redefined-builtin
 """
 Module for communication with gvmd in Greenbone Management Protocol version 7
 """
