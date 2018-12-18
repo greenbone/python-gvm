225361bcb65a8eb1017a24a39afaf551542bd189
Michael Wiegand <michael.wiegand@greenbone.net>
2018-12-18 09:23:17 +0100

    Increase coverage of get_nvts test case

--- tests/protocols/gmpv7/test_get_nvts.py
+++ tests/protocols/gmpv7/test_get_nvts.py
@@ -22,6 +22,7 @@ from gvm.protocols.gmpv7 import Gmp
 
 from .. import MockConnection
 
+
 class GmpGetNvtsTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -33,6 +34,69 @@ class GmpGetNvtsTestCase(unittest.TestCase):
 
         self.connection.send.has_been_called_with(
             '<get_nvts/>')
+
+    def test_get_nvts_with_details(self):
+        self.gmp.get_nvts(details=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts details="1"/>'
+        )
+
+    def test_get_nvts_with_preferences(self):
+        self.gmp.get_nvts(preferences=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts preferences="1"/>'
+        )
+
+    def test_get_nvts_with_preference_count(self):
+        self.gmp.get_nvts(preference_count=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts preference_count="1"/>'
+        )
+
+    def test_get_nvts_with_timeout(self):
+        self.gmp.get_nvts(timeout=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts timeout="1"/>'
+        )
+
+    def test_get_nvts_with_config_id(self):
+        self.gmp.get_nvts(config_id='config_id')
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts config_id="config_id"/>'
+        )
+
+    def test_get_nvts_with_preferences_config_id(self):
+        self.gmp.get_nvts(preferences_config_id='preferences_config_id')
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts preferences_config_id="preferences_config_id"/>'
+        )
+
+    def test_get_nvts_with_family(self):
+        self.gmp.get_nvts(family='family')
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts family="family"/>'
+        )
+
+    def test_get_nvts_with_sort_order(self):
+        self.gmp.get_nvts(sort_order='sort_order')
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts sort_order="sort_order"/>'
+        )
+
+    def test_get_nvts_with_sort_field(self):
+        self.gmp.get_nvts(sort_field='sort_field')
+
+        self.connection.send.has_been_called_with(
+            '<get_nvts sort_field="sort_field"/>'
+        )
 
 
 if __name__ == '__main__':
