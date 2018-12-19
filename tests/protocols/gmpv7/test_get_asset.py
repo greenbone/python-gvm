1023ac73f1ea2b52a40cdaf49fb2a89fffef35ff
Björn Ricks <bjoern.ricks@greenbone.net>
2018-12-19 11:57:47 +0100

    Add tests for get_asset method

---
+++ tests/protocols/gmpv7/test_get_asset.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from gvm.errors import RequiredArgument, InvalidArgument
+from gvm.protocols.gmpv7 import Gmp
+
+from .. import MockConnection
+
+
+class GmpGetAssetTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.connection = MockConnection()
+        self.gmp = Gmp(self.connection)
+
+    def test_get_asset_host(self):
+        self.gmp.get_asset('a1', 'host')
+
+        self.connection.send.has_been_called_with(
+            '<get_assets asset_id="a1" type="host"/>'
+        )
+
+        self.gmp.get_asset(asset_id='a1', asset_type='host')
+
+        self.connection.send.has_been_called_with(
+            '<get_assets asset_id="a1" type="host"/>'
+        )
+
+    def test_get_asset_os(self):
+        self.gmp.get_asset('a1', 'os')
+
+        self.connection.send.has_been_called_with(
+            '<get_assets asset_id="a1" type="os"/>'
+        )
+        self.gmp.get_asset(asset_id='a1', asset_type='os')
+
+        self.connection.send.has_been_called_with(
+            '<get_assets asset_id="a1" type="os"/>'
+        )
+
+    def test_get_asset_missing_asset_id(self):
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_asset(asset_id=None, asset_type='host')
+
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_asset(asset_id='', asset_type='os')
+
+    def test_get_asset_invalid_asset_type(self):
+        with self.assertRaises(InvalidArgument):
+            self.gmp.get_asset(asset_id='a1', asset_type='foo')
+
+        with self.assertRaises(InvalidArgument):
+            self.gmp.get_asset(asset_id='a1', asset_type=None)
+
+        with self.assertRaises(InvalidArgument):
+            self.gmp.get_asset(asset_id='a1', asset_type='')
+
+
+if __name__ == '__main__':
+    unittest.main()
