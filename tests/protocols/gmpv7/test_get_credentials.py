5b454a3e2c0659487833d729c3fee79b0ac4b66e
Björn Ricks <bjoern.ricks@greenbone.net>
2018-12-19 12:25:56 +0100

    Add additional tests for get_credentials

--- tests/protocols/gmpv7/test_get_credentials.py
+++ tests/protocols/gmpv7/test_get_credentials.py
@@ -28,11 +28,65 @@ class GmpGetCredentialsTestCase(unittest.TestCase):
         self.connection = MockConnection()
         self.gmp = Gmp(self.connection)
 
-    def test_get_credentials_simple(self):
+    def test_get_credential(self):
         self.gmp.get_credentials()
 
         self.connection.send.has_been_called_with(
             '<get_credentials/>')
+
+    def test_get_credentials_with_filter(self):
+        self.gmp.get_credentials(filter='foo=bar')
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials filter="foo=bar"/>'
+        )
+
+    def test_get_credentials_with_filter_id(self):
+        self.gmp.get_credentials(filter_id='f1')
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials filt_id="f1"/>'
+        )
+
+    def test_get_credentials_with_scanners(self):
+        self.gmp.get_credentials(scanners=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials scanners="1"/>'
+        )
+
+        self.gmp.get_credentials(scanners=False)
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials scanners="0"/>'
+        )
+
+    def test_get_credentials_with_trash(self):
+        self.gmp.get_credentials(trash=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials trash="1"/>'
+        )
+
+        self.gmp.get_credentials(trash=False)
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials trash="0"/>'
+        )
+
+    def test_get_credentials_with_targets(self):
+        self.gmp.get_credentials(targets=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials targets="1"/>'
+        )
+
+        self.gmp.get_credentials(targets=False)
+
+        self.connection.send.has_been_called_with(
+            '<get_credentials targets="0"/>'
+        )
+
 
 
 if __name__ == '__main__':
