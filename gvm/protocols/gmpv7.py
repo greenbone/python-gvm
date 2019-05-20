a1d7252e82dc5cc82ac447146e10c96beb83af1f
Kars de Jong <jongk@linux-m68k.org>
2019-05-20 10:32:05 +0200

    Rename "details" parameter of "get_reports" to "no_details" to make its meaning clearer

--- gvm/protocols/gmpv7.py
+++ gvm/protocols/gmpv7.py
@@ -3373,7 +3373,7 @@ class Gmp(GvmProtocol):
         filter_id=None,
         note_details=None,
         override_details=None,
-        details=None
+        no_details=None
     ):
         """Request a list of reports
 
@@ -3385,7 +3385,7 @@ class Gmp(GvmProtocol):
                 include note details
             override_details (boolean, optional): If overrides are included,
                 whether to include override details
-            details (boolean, optional): Whether to exclude results
+            no_details (boolean, optional): Whether to exclude results
 
         Returns:
             The response. See :py:meth:`send_command` for details.
@@ -3404,8 +3404,8 @@ class Gmp(GvmProtocol):
         if not override_details is None:
             cmd.set_attribute("override_details", _to_bool(override_details))
 
-        if not details is None:
-            cmd.set_attribute("details", _to_bool(details))
+        if not no_details is None:
+            cmd.set_attribute("details", _to_bool(not no_details))
 
         cmd.set_attribute("ignore_pagination", "1")
 
--- tests/protocols/gmpv7/test_get_reports.py
+++ tests/protocols/gmpv7/test_get_reports.py
@@ -77,18 +77,18 @@ class GmpGetReportsTestCase(unittest.TestCase):
             '<get_reports override_details="1" ignore_pagination="1"/>'
         )
 
-    def test_get_reports_without_details(self):
-        self.gmp.get_reports(details=False)
-
-        self.connection.send.has_been_called_with(
-            '<get_reports details="0" ignore_pagination="1"/>'
-        )
-
     def test_get_reports_with_details(self):
-        self.gmp.get_reports(details=True)
+        self.gmp.get_reports(no_details=False)
 
         self.connection.send.has_been_called_with(
             '<get_reports details="1" ignore_pagination="1"/>'
+        )
+
+    def test_get_reports_without_details(self):
+        self.gmp.get_reports(no_details=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_reports details="0" ignore_pagination="1"/>'
         )
 
 
