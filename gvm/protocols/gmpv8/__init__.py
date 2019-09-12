7e292e0b1c2699a79926c0280f540b7466fc6873
jstach <jstach@uos.de>
2019-09-12 19:56:03 +0200

    Corrected the resource_type type from String to EntityType, added conditions to check the type

--- gvm/protocols/gmpv8/__init__.py
+++ gvm/protocols/gmpv8/__init__.py
@@ -429,7 +429,7 @@ class Gmp(Gmpv7):
     def create_tag(
         self,
         name: str,
-        resource_type: str,
+        resource_type: EntityType,
         *,
         resource_filter: Optional[str] = None,
         resource_ids: Optional[List[str]] = None,
@@ -470,6 +470,10 @@ class Gmp(Gmpv7):
                 function="create_tag", argument="resource_type"
             )
 
+        if not isinstance(resource_type, EntityType):
+            raise InvalidArgument(function="create_tag", argument="resource_type")
+
+
         cmd = XmlCommand('create_tag')
         cmd.add_element('name', name)
 
@@ -482,7 +486,7 @@ class Gmp(Gmpv7):
                 "resource", attrs={"id": str(resource_id)}
             )
 
-        _xmlresources.add_element("type", resource_type)
+        _xmlresources.add_element("type", resource_type.value)
 
         if comment:
             cmd.add_element("comment", comment)
@@ -507,7 +511,7 @@ class Gmp(Gmpv7):
         value=None,
         active=None,
         resource_action: Optional[str] = None,
-        resource_type: Optional[str] = None,
+        resource_type: Optional[EntityType] = None,
         resource_filter: Optional[str] = None,
         resource_ids: Optional[List[str]] = None
     ) -> Any:
@@ -570,7 +574,9 @@ class Gmp(Gmpv7):
                 )
 
             if resource_type is not None:
-                _xmlresources.add_element("type", resource_type)
+                if not isinstance(resource_type, EntityType):
+                    raise InvalidArgument(function="modify_tag", argument="resource_type")
+                _xmlresources.add_element("type", resource_type.value)
 
         return self._send_xml_command(cmd)
 
