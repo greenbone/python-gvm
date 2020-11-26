8d5bb1d9919cc10ae2f4af1f24bd49787f5cd86c
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-11-26 10:24:11 +0100

    Improve modify_user function for gmpv7
    Add parameters comment, auth_source and group_ids.
    Make documentation about hosts_allow and
    ifaces_allow clearer.

--- gvm/protocols/gmpv7/gmpv7.py
+++ gvm/protocols/gmpv7/gmpv7.py
@@ -6070,12 +6070,15 @@ class GmpV7Mixin(GvmProtocol):
         name: str = None,
         *,
         new_name: Optional[str] = None,
+        comment: Optional[str] = None,
         password: Optional[str] = None,
+        auth_source: Optional[UserAuthType] = None,
         role_ids: Optional[List[str]] = None,
         hosts: Optional[List[str]] = None,
         hosts_allow: Optional[bool] = False,
         ifaces: Optional[List[str]] = None,
-        ifaces_allow: Optional[bool] = False
+        ifaces_allow: Optional[bool] = False,
+        group_ids: Optional[List[str]] = None,
     ) -> Any:
         """Modifies an existing user.
 
@@ -6085,12 +6088,25 @@ class GmpV7Mixin(GvmProtocol):
             name: The name of the user to be modified. Either user_id or name
                 must be passed.
             new_name: The new name for the user.
+            comment: Comment on the user.
             password: The password for the user.
+            auth_source: Source allowed for authentication for this user.
             roles_id: List of roles UUIDs for the user.
             hosts: User access rules: List of hosts.
-            hosts_allow: If True, allow only listed, otherwise forbid listed.
+            hosts_allow: Defines how the hosts list is to be interpreted.
+                If False (default) the list is treated as a deny list.
+                All hosts are allowed by default except those provided by
+                the hosts parameter. If True the list is treated as a
+                allow list. All hosts are denied by default except those
+                provided by the hosts parameter.
             ifaces: User access rules: List of ifaces.
-            ifaces_allow: If True, allow only listed, otherwise forbid listed.
+            ifaces_allow: Defines how the ifaces list is to be interpreted.
+                If False (default) the list is treated as a deny list.
+                All ifaces are allowed by default except those provided by
+                the ifaces parameter. If True the list is treated as a
+                allow list. All ifaces are denied by default except those
+                provided by the ifaces parameter.
+            group_ids: List of group UUIDs for the user.
 
         Returns:
             The response. See :py:meth:`send_command` for details.
@@ -6110,9 +6126,6 @@ class GmpV7Mixin(GvmProtocol):
         if new_name:
             cmd.add_element("new_name", new_name)
 
-        if password:
-            cmd.add_element("password", password)
-
         if role_ids:
             for role in role_ids:
                 cmd.add_element("role", attrs={"id": role})
@@ -6130,6 +6143,21 @@ class GmpV7Mixin(GvmProtocol):
                 _to_comma_list(ifaces),
                 attrs={"allow": _to_bool(ifaces_allow)},
             )
+
+        if comment:
+            cmd.add_element("comment", comment)
+
+        if password:
+            cmd.add_element("password", password)
+
+        if auth_source:
+            _xmlauthsrc = cmd.add_element("sources")
+            _xmlauthsrc.add_element("source", auth_source.value)
+
+        if group_ids:
+            _xmlgroups = cmd.add_element("groups")
+            for group_id in group_ids:
+                _xmlgroups.add_element("group", attrs={"id": group_id})
 
         return self._send_xml_command(cmd)
 
