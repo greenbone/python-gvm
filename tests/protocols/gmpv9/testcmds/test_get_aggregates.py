b66bd7fda847b158e878e26b7b95834f8e5b5fc9
Timo Pollmeier <timo.pollmeier@greenbone.net>
2020-12-08 14:33:35 +0100

    Update tests for get_aggregates
    The tests now test the explicitly defined parameters, not just
    resource_type and a generic kwargs.

--- tests/protocols/gmpv9/testcmds/test_get_aggregates.py
+++ tests/protocols/gmpv9/testcmds/test_get_aggregates.py
@@ -25,6 +25,10 @@ from gvm.protocols.gmpv9 import EntityType
 
 class GmpGetAggregatesTestCase:
     def test_get_aggregates(self):
+        """
+        Test basic get_aggregates calls with only resource_type except special
+        cases for audit, policy, scan_config and task.
+        """
         self.gmp.get_aggregates(EntityType.ALERT)
 
         self.connection.send.has_been_called_with(
@@ -101,20 +105,39 @@ class GmpGetAggregatesTestCase:
             '<get_aggregates type="result"/>'
         )
 
+    def test_get_aggregates_resource_types_with_usage_type(self):
+        """
+        Test special cases of resource_type in get_aggregates calls that
+        should add a usage_type parameter: audit, policy, scan_config and task.
+        """
+        self.gmp.get_aggregates(EntityType.AUDIT)
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates usage_type="audit" type="task"/>'
+        )
+
+        self.gmp.get_aggregates(EntityType.POLICY)
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates usage_type="policy" type="config"/>'
+        )
+
+        self.gmp.get_aggregates(EntityType.SCAN_CONFIG)
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates usage_type="scan" type="config"/>'
+        )
+
         self.gmp.get_aggregates(EntityType.TASK)
 
         self.connection.send.has_been_called_with(
-            '<get_aggregates type="task"/>'
-        )
-
-    def test_get_aggregates_kwargs(self):
-        self.gmp.get_aggregates(EntityType.ALERT, group_column="family")
-
-        self.connection.send.has_been_called_with(
-            '<get_aggregates type="alert" group_column="family"/>'
+            '<get_aggregates usage_type="scan" type="task"/>'
         )
 
     def test_get_aggregates_missing_resource_type(self):
+        """
+        Test get_aggregates calls with missing resource_type
+        """
         with self.assertRaises(RequiredArgument):
             self.gmp.get_aggregates(resource_type=None)
 
@@ -125,9 +148,137 @@ class GmpGetAggregatesTestCase:
             self.gmp.get_aggregates('')
 
     def test_get_aggregates_invalid_resource_type(self):
+        """
+        Test get_aggregates calls with invalid resource_type
+        """
         with self.assertRaises(InvalidArgumentType):
             self.gmp.get_aggregates(resource_type='foo')
 
+    def test_get_aggregates_sort_criteria(self):
+        """
+        Test get_aggregates calls with sort_criteria
+        """
+        self.gmp.get_aggregates(
+            EntityType.NVT,
+            group_column='family',
+            sort_criteria=[
+                {'field': 'severity', 'stat': 'mean', 'order': 'descending'},
+                {'stat': 'count', 'order': 'descending'},
+                {'field': 'family', 'order': 'ascending'},
+            ],
+            data_columns=['severity'],
+        )
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates type="nvt" group_column="family">'
+            '<sort field="severity" stat="mean" order="descending"/>'
+            '<sort stat="count" order="descending"/>'
+            '<sort field="family" order="ascending"/>'
+            '<data_column>severity</data_column>'
+            '</get_aggregates>'
+        )
+
+    def test_get_aggregates_invalid_sort_criteria(self):
+        """
+        Test get_aggregates calls with invalid sort_criteria
+        """
+        with self.assertRaises(InvalidArgumentType):
+            self.gmp.get_aggregates(
+                resource_type=EntityType.ALERT, sort_criteria='INVALID'
+            )
+
+    def test_get_aggregates_data_columns(self):
+        """
+        Test get_aggregates calls with data_columns
+        """
+        self.gmp.get_aggregates(
+            EntityType.CPE, data_columns=['severity', 'cves']
+        )
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates type="cpe">'
+            '<data_column>severity</data_column>'
+            '<data_column>cves</data_column>'
+            '</get_aggregates>'
+        )
+
+    def test_get_aggregates_invalid_data_columns(self):
+        """
+        Test get_aggregates calls with invalid data_columns
+        """
+        with self.assertRaises(InvalidArgumentType):
+            self.gmp.get_aggregates(
+                resource_type=EntityType.ALERT, data_columns='INVALID'
+            )
+
+    def test_get_aggregates_group_column(self):
+        """
+        Test get_aggregates calls with group_column
+        """
+        self.gmp.get_aggregates(EntityType.NVT, group_column='family')
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates type="nvt" group_column="family"/>'
+        )
+
+    def test_get_aggregates_subgroup_column(self):
+        """
+        Test get_aggregates calls with subgroup_column
+        """
+        self.gmp.get_aggregates(
+            EntityType.NVT,
+            group_column='family',
+            subgroup_column='solution_type',
+        )
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates type="nvt" group_column="family"'
+            ' subgroup_column="solution_type"/>'
+        )
+
+    def test_get_aggregates_missing_group_column(self):
+        """
+        Test get_aggregates calls with group_column missing
+        if subgroup_column was given.
+        """
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_aggregates(
+                resource_type=EntityType.NVT, subgroup_column='solution_type'
+            )
+
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_aggregates(
+                resource_type=EntityType.NVT,
+                group_column='',
+                subgroup_column='solution_type',
+            )
+
+    def test_get_aggregates_text_columns(self):
+        """
+        Test get_aggregates calls with text_columns
+        """
+        self.gmp.get_aggregates(
+            EntityType.SCAN_CONFIG,
+            group_column="uuid",
+            text_columns=['name', 'comment']
+        )
+
+        self.connection.send.has_been_called_with(
+            '<get_aggregates usage_type="scan" type="config"'
+            ' group_column="uuid">'
+            '<text_column>name</text_column>'
+            '<text_column>comment</text_column>'
+            '</get_aggregates>'
+        )
+
+    def test_get_aggregates_invalid_text_columns(self):
+        """
+        Test get_aggregates calls with invalid text_columns
+        """
+        with self.assertRaises(InvalidArgumentType):
+            self.gmp.get_aggregates(
+                resource_type=EntityType.ALERT, text_columns='INVALID'
+            )
 
 if __name__ == '__main__':
     unittest.main()
