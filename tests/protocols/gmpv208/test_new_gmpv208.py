8d23bc9894b643d9f58b796cd8277088025ddf9a
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-04-27 10:55:41 +0200

    Adding testcases from v9 to v20.08

--- tests/protocols/gmpv208/test_new_gmpv208.py
+++ tests/protocols/gmpv208/test_new_gmpv208.py
@@ -78,3 +78,224 @@ class Gmpv208ModifyTagTestCase(GmpModifyTagTestCase, Gmpv208TestCase):
 
 class Gmpv208GetFeedTestCase(GmpGetFeedTestCase, Gmpv208TestCase):
     pass
+
+
+class Gmpv208CloneAuditTestCase(GmpCloneAuditTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CloneConfigTestCase(GmpCloneConfigTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208ClonePolicyTestCase(GmpClonePolicyTestCase, Gmpv208TestCase):
+
+    pass
+
+
+class Gmpv208CloneTaskTestCase(GmpCloneTaskTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CloneTLSCertificateTestCase(
+    GmpCloneTLSCertificateTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208CreateAuditCommandTestCase(
+    GmpCreateAuditCommandTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208CreateConfigTestCase(GmpCreateConfigTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CreateConfigFromOSPScannerTestCase(
+    GmpCreateConfigFromOSPScannerTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208CreateCredentialTestCase(
+    GmpCreateCredentialTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208CreatePolicyTestCase(GmpCreatePolicyTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CreateTaskCommandTestCase(
+    GmpCreateTaskCommandTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyAlertTestCase(GmpModifyAlertTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CreateAlertTestCase(GmpCreateAlertTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CreateTLSCertificateTestCase(
+    GmpCreateTLSCertificateTestCase, Gmpv208TestCase
+):
+
+    pass
+
+
+class Gmpv208DeleteAuditTestCase(GmpDeleteAuditTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208DeleteConfigTestCase(GmpDeleteConfigTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208DeletePolicyTestCase(GmpDeletePolicyTestCase, Gmpv208TestCase):
+
+    pass
+
+
+class Gmpv208DeleteTaskTestCase(GmpDeleteTaskTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208DeleteTLSCertificateTestCase(
+    GmpDeleteTLSCertificateTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208GetAuditTestCase(GmpGetAuditTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetAuditsTestCase(GmpGetAuditsTestCase, Gmpv208TestCase):
+
+    pass
+
+
+class Gmpv208GetConfigTestCase(GmpGetConfigTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetConfigsTestCase(GmpGetConfigsTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetPoliciesTestCase(GmpGetPoliciesTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetPolicyTestCase(GmpGetPolicyTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetTaskTestCase(GmpGetTaskTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetTasksTestCase(GmpGetTasksTestCase, Gmpv208TestCase):
+
+    pass
+
+
+class Gmpv208GetTlsCertificateTestCase(
+    GmpGetTlsCertificateTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208GetTLSCertificatesTestCase(
+    GmpGetTLSCertificatesTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyAuditCommandTestCase(
+    GmpModifyAuditCommandTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyCredentialTestCase(
+    GmpModifyCredentialTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyPolicySetCommentTestCase(
+    GmpModifyPolicySetCommentTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyPolicySetNameTestCase(
+    GmpModifyPolicySetNameTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyPolicySetFamilySelectionTestCase(
+    GmpModifyPolicySetFamilySelectionTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyPolicySetNvtPreferenceTestCase(
+    GmpModifyPolicySetNvtPreferenceTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyPolicySetNvtSelectionTestCase(
+    GmpModifyPolicySetNvtSelectionTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyPolicySetScannerPreferenceTestCase(
+    GmpModifyPolicySetScannerPreferenceTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208ModifyTicketTestCase(GmpModifyTicketTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208ModifyTLSCertificateTestCase(
+    GmpModifyTLSCertificateTestCase, Gmpv208TestCase
+):
+    pass
+
+
+class Gmpv208CreateScannerTestCase(GmpCreateScannerTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208ModifyScannerTestCase(GmpModifyScannerTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208ResumeAuditTestCase(GmpResumeAuditTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208StartAuditTestCase(GmpStartAuditTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208StopAuditTestCase(GmpStopAuditTestCase, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208ImportReportTestCase(GmpImportReportTestCase, Gmpv208TestCase):
+    pass
--- tests/protocols/gmpv208/testcmds/__init__.py
+++ tests/protocols/gmpv208/testcmds/__init__.py
@@ -31,3 +31,58 @@ from .test_modify_permission import GmpModifyPermissionTestCase
 from .test_modify_report_format import GmpModifyReportFormatTestCase
 from .test_modify_tag import GmpModifyTagTestCase
 from .test_get_feed import GmpGetFeedTestCase
+from .test_clone_audit import GmpCloneAuditTestCase
+from .test_clone_config import GmpCloneConfigTestCase
+from .test_clone_policy import GmpClonePolicyTestCase
+from .test_clone_task import GmpCloneTaskTestCase
+from .test_clone_tls_certificate import GmpCloneTLSCertificateTestCase
+from .test_create_audit import GmpCreateAuditCommandTestCase
+from .test_create_alert import GmpCreateAlertTestCase
+from .test_create_config import GmpCreateConfigTestCase
+from .test_create_config_from_osp_scanner import (
+    GmpCreateConfigFromOSPScannerTestCase,
+)
+from .test_create_credential import GmpCreateCredentialTestCase
+from .test_create_policy import GmpCreatePolicyTestCase
+from .test_create_scanner import GmpCreateScannerTestCase
+from .test_create_task import GmpCreateTaskCommandTestCase
+from .test_create_tls_certificate import GmpCreateTLSCertificateTestCase
+from .test_delete_audit import GmpDeleteAuditTestCase
+from .test_delete_config import GmpDeleteConfigTestCase
+from .test_delete_policy import GmpDeletePolicyTestCase
+from .test_delete_task import GmpDeleteTaskTestCase
+from .test_delete_tls_certificate import GmpDeleteTLSCertificateTestCase
+from .test_get_audit import GmpGetAuditTestCase
+from .test_get_audits import GmpGetAuditsTestCase
+from .test_get_config import GmpGetConfigTestCase
+from .test_get_configs import GmpGetConfigsTestCase
+from .test_get_policies import GmpGetPoliciesTestCase
+from .test_get_policy import GmpGetPolicyTestCase
+from .test_get_task import GmpGetTaskTestCase
+from .test_get_tasks import GmpGetTasksTestCase
+from .test_get_tls_certificate import GmpGetTlsCertificateTestCase
+from .test_get_tls_certificates import GmpGetTLSCertificatesTestCase
+from .test_modify_credential import GmpModifyCredentialTestCase
+from .test_modify_alert import GmpModifyAlertTestCase
+from .test_modify_audit import GmpModifyAuditCommandTestCase
+from .test_modify_policy_set_comment import GmpModifyPolicySetCommentTestCase
+from .test_modify_policy_set_family_selection import (
+    GmpModifyPolicySetFamilySelectionTestCase,
+)
+from .test_modify_policy_set_name import GmpModifyPolicySetNameTestCase
+from .test_modify_policy_set_nvt_preference import (
+    GmpModifyPolicySetNvtPreferenceTestCase,
+)
+from .test_modify_policy_set_nvt_selection import (
+    GmpModifyPolicySetNvtSelectionTestCase,
+)
+from .test_modify_policy_set_scanner_preference import (
+    GmpModifyPolicySetScannerPreferenceTestCase,
+)
+from .test_modify_scanner import GmpModifyScannerTestCase
+from .test_modify_ticket import GmpModifyTicketTestCase
+from .test_modify_tls_certificate import GmpModifyTLSCertificateTestCase
+from .test_resume_audit import GmpResumeAuditTestCase
+from .test_start_audit import GmpStartAuditTestCase
+from .test_stop_audit import GmpStopAuditTestCase
+from .test_import_report import GmpImportReportTestCase
