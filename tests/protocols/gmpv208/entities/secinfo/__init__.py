9c4e22110aa02b9ae2eda21262364257eb83f58c
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-05-11 13:00:43 +0200

    Add more testcases

--- tests/protocols/gmpv208/entities/secinfo/__init__.py
+++ tests/protocols/gmpv208/entities/secinfo/__init__.py
@@ -17,12 +17,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from .test_get_cert_bund_advisory import GmpGetCertBundTestMixin
+from .test_get_cert_bund_advisory_list import GmpGetCertBundListTestMixin
 from .test_get_cpe import GmpGetCpeTestMixin
+from .test_get_cpe_list import GmpGetCpeListTestMixin
 from .test_get_cve import GmpGetCveTestMixin
+from .test_get_cve_list import GmpGetCveListTestMixin
 from .test_get_dfn_cert_advisory import GmpGetDfnCertTestMixin
+from .test_get_dfn_cert_advisory_list import GmpGetDfnCertListTestMixin
 from .test_get_info import GmpGetInfoTestMixin
 from .test_get_info_list import GmpGetInfoListTestMixin
 from .test_get_nvt import GmpGetNvtTestMixin
+from .test_get_nvt_list import GmpGetNvtListTestMixin
 from .test_get_nvt_families import GmpGetNvtFamiliesTestMixin
+from .test_get_oval_definition import GmpGetOvalDefTestMixin
+from .test_get_oval_definition_list import GmpGetOvalDefListTestMixin
 from .test_get_scan_config_nvt import GmpGetScanConfigNvtTestMixin
 from .test_get_scan_config_nvts import GmpGetScanConfigNvtsTestMixin
---
+++ tests/protocols/gmpv208/entities/secinfo/test_get_oval_definition_list.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class GmpGetOvalDefListTestMixin:
+    def test_get_oval_definition_list(self):
+        self.gmp.get_oval_definition_list()
+
+        self.connection.send.has_been_called_with('<get_info type="CVE"/>')
+
+    def test_get_oval_definition_list_with_filter(self):
+        self.gmp.get_oval_definition_list(filter='foo=bar')
+
+        self.connection.send.has_been_called_with(
+            '<get_info type="CVE" filter="foo=bar"/>'
+        )
+
+    def test_get_oval_definition_list_with_filter_id(self):
+        self.gmp.get_oval_definition_list(filter_id='f1')
+
+        self.connection.send.has_been_called_with(
+            '<get_info type="CVE" filt_id="f1"/>'
+        )
+
+    def test_get_oval_definition_list_with_name(self):
+        self.gmp.get_oval_definition_list(name='foo')
+
+        self.connection.send.has_been_called_with(
+            '<get_info type="CVE" name="foo"/>'
+        )
+
+    def test_get_oval_definition_list_with_details(self):
+        self.gmp.get_oval_definition_list(details=True)
+
+        self.connection.send.has_been_called_with(
+            '<get_info type="CVE" details="1"/>'
+        )
+
+        self.gmp.get_oval_definition_list(details=False)
+
+        self.connection.send.has_been_called_with(
+            '<get_info type="CVE" details="0"/>'
+        )
