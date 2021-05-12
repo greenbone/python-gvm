1ae477a800eebbee44da66245166ce2bd4e2a302
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-05-12 14:47:44 +0200

    Added files for the actual tests for 208 and 214

---
+++ tests/protocols/gmpv214/entities/test_notes.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ...gmpv208 import Gmpv208TestCase
+from ...gmpv208.entities.notes import (
+    GmpCloneNoteTestMixin,
+    GmpDeleteNoteTestMixin,
+    GmpGetNoteTestMixin,
+    GmpGetNotesTestMixin,
+)
+from .notes import (
+    GmpCreateNoteTestMixin,
+    GmpModifyNoteTestMixin,
+)
+
+
+class Gmpv208DeleteNoteTestCase(GmpDeleteNoteTestMixin, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetNoteTestCase(GmpGetNoteTestMixin, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208GetNotesTestCase(GmpGetNotesTestMixin, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CloneNoteTestCase(GmpCloneNoteTestMixin, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208CreateNoteTestCase(GmpCreateNoteTestMixin, Gmpv208TestCase):
+    pass
+
+
+class Gmpv208ModifyNoteTestCase(GmpModifyNoteTestMixin, Gmpv208TestCase):
+    pass
