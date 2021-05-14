2405a49bef6cbbfa38fe56601fc91b198c9df56b
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-05-14 14:58:42 +0200

    Add OS tests

---
+++ tests/protocols/gmpv208/entities/operating_systems/test_delete_operating_system.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gvm.errors import GvmError
+
+
+class GmpDeleteOperatingSystemTestMixin:
+    def test_delete_operating_system(self):
+        self.gmp.delete_operating_system(operating_system_id='a1')
+
+        self.connection.send.has_been_called_with(
+            '<delete_asset asset_id="a1"/>'
+        )
+
+    def test_delete_operating_systems_from_report(self):
+        self.gmp.delete_operating_system(report_id='a1')
+
+        self.connection.send.has_been_called_with(
+            '<delete_asset report_id="a1"/>'
+        )
+
+    def test_delete_passing_asset_and_report(self):
+        self.gmp.delete_operating_system(
+            report_id='r1', operating_system_id='a1'
+        )
+
+        self.connection.send.has_been_called_with(
+            '<delete_asset asset_id="a1"/>'
+        )
+
+    def test_missing_arguments(self):
+        with self.assertRaises(GvmError):
+            self.gmp.delete_operating_system()
---
+++ tests/protocols/gmpv208/entities/operating_systems/test_modify_operating_system.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gvm.errors import RequiredArgument
+
+
+class GmpModifyOperatingSystemTestMixin:
+    def test_modify_operating_system(self):
+        self.gmp.modify_operating_system(operating_system_id='a1')
+
+        self.connection.send.has_been_called_with(
+            '<modify_asset asset_id="a1">'
+            '<comment></comment>'
+            '</modify_asset>'
+        )
+
+    def test_modify_operating_system_without_operating_system_id(self):
+        with self.assertRaises(RequiredArgument):
+            self.gmp.modify_operating_system(
+                operating_system_id=None, comment='foo'
+            )
+
+        with self.assertRaises(RequiredArgument):
+            self.gmp.modify_operating_system(
+                operating_system_id='', comment='foo'
+            )
+
+        with self.assertRaises(RequiredArgument):
+            self.gmp.modify_operating_system('', comment='foo')
+
+    def test_modify_operating_system_with_comment(self):
+        self.gmp.modify_operating_system('a1', 'foo')
+
+        self.connection.send.has_been_called_with(
+            '<modify_asset asset_id="a1">'
+            '<comment>foo</comment>'
+            '</modify_asset>'
+        )
+
+        self.gmp.modify_operating_system('a1', comment='foo')
+
+        self.connection.send.has_been_called_with(
+            '<modify_asset asset_id="a1">'
+            '<comment>foo</comment>'
+            '</modify_asset>'
+        )
+
+        self.gmp.modify_operating_system('a1', '')
+
+        self.connection.send.has_been_called_with(
+            '<modify_asset asset_id="a1">'
+            '<comment></comment>'
+            '</modify_asset>'
+        )
+
+        self.gmp.modify_operating_system('a1', None)
+
+        self.connection.send.has_been_called_with(
+            '<modify_asset asset_id="a1">' '<comment/>' '</modify_asset>'
+        )
