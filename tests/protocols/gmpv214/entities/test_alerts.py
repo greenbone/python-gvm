b9b32af115fb8e9f81bb527ebbd98a6c71acc26a
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-05-14 09:42:38 +0200

    Add the new test cases

---
+++ tests/protocols/gmpv214/entities/test_alerts.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ...gmpv214 import Gmpv214TestCase
+from ...gmpv208.entities.alerts import (
+    GmpCloneAlertTestMixin,
+    GmpCreateAlertTestMixin,
+    GmpDeleteAlertTestMixin,
+    GmpGetAlertsTestMixin,
+    GmpGetAlertTestMixin,
+    GmpModifyAlertTestMixin,
+    GmpTestAlertTestMixin,
+    GmpTriggerAlertTestMixin,
+)
+
+
+class Gmpv214CloneAlertTestCase(GmpCloneAlertTestMixin, Gmpv214TestCase):
+    pass
+
+
+class Gmpv214CreateAlertTestCase(GmpCreateAlertTestMixin, Gmpv214TestCase):
+    pass
+
+
+class Gmpv214DeleteAlertTestCase(GmpDeleteAlertTestMixin, Gmpv214TestCase):
+    pass
+
+
+class Gmpv214GetAlertTestCase(GmpGetAlertTestMixin, Gmpv214TestCase):
+    pass
+
+
+class Gmpv214GetAlertsTestCase(GmpGetAlertsTestMixin, Gmpv214TestCase):
+    pass
+
+
+class Gmpv214ModifyAlertTestCase(GmpModifyAlertTestMixin, Gmpv214TestCase):
+    pass
+
+
+class Gmpv214TestAlertTestCase(GmpTestAlertTestMixin, Gmpv214TestCase):
+    pass
+
+
+class Gmpv214TriggerAlertTestCase(GmpTriggerAlertTestMixin, Gmpv214TestCase):
+    pass
