3b02d0c32a5a6f57bd044bf4cdbf92199cfe8782
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-05-17 13:11:06 +0200

    Increase create_policy coverage

--- tests/protocols/gmpv208/entities/policies/test_create_policy.py
+++ tests/protocols/gmpv208/entities/policies/test_create_policy.py
@@ -31,11 +31,12 @@ class GmpCreatePolicyTestMixin:
             '</create_config>'
         )
 
-    def test_create_with_policy_id(self):
-        self.gmp.create_policy('foo', policy_id='p1')
+    def test_create_with_policy_id_and_comment(self):
+        self.gmp.create_policy('foo', policy_id='p1', comment="foo")
 
         self.connection.send.has_been_called_with(
             '<create_config>'
+            '<comment>foo</comment>'
             '<copy>p1</copy>'
             '<name>foo</name>'
             '<usage_type>policy</usage_type>'
