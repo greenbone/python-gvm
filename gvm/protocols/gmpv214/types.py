f89fb26cb2bd31ebf385c1a76a3ea5eaed19099a
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-05-19 09:52:26 +0200

    Remove 214 SeverityLevel, the corrosponding parameters in overrides are deprecated

--- gvm/protocols/gmpv214/types.py
+++ gvm/protocols/gmpv214/types.py
@@ -15,11 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from enum import Enum
-
-from typing import Optional
-
-from gvm.errors import InvalidArgument
 
 from gvm.protocols.gmpv208.types import (
     FilterType,
@@ -36,38 +31,12 @@ from gvm.protocols.gmpv208.types import (
 __all__ = [
     "FilterType",
     "HostsOrdering",
-    "SeverityLevel",
     "TicketStatus",
     "TimeUnit",
     "get_filter_type_from_string",
     "get_hosts_ordering_from_string",
-    "get_severity_level_from_string",
     "get_ticket_status_from_string",
     "get_time_unit_from_string",
 ]
 
 # move this: Severity Level !!!
-class SeverityLevel(Enum):
-    """Enum for severity levels"""
-
-    HIGH = "High"
-    MEDIUM = "Medium"
-    LOW = "Low"
-    LOG = "Log"
-    ALARM = "Alarm"
-
-
-def get_severity_level_from_string(
-    severity_level: Optional[str],
-) -> Optional[SeverityLevel]:
-    """Convert a severity level string into a SeverityLevel instance"""
-    if not severity_level:
-        return None
-
-    try:
-        return SeverityLevel[severity_level.upper()]
-    except KeyError:
-        raise InvalidArgument(
-            argument='severity_level',
-            function=get_severity_level_from_string.__name__,
-        ) from None
