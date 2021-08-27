71d7b55bb23e62ae79c3b8ae68e0c41f6bb3d303
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-08-27 17:15:33 +0200

    Fixing pylint by disable

--- gvm/protocols/gmpv214/entities/users.py
+++ gvm/protocols/gmpv214/entities/users.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=arguments-differ
+# pylint: disable=arguments-differ, arguments-renamed
 
 from typing import Any, List, Optional
 
