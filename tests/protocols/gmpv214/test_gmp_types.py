65ba64546e19da4c191c833d2c14ad026fc3eca4
Björn Ricks <bjoern.ricks@greenbone.net>
2023-03-28 16:50:46 +0200

    Fix: Resolve deprecation warnings about assertEquals in tests
    assertEquals is deprecated and assertEqual should be used instead.

--- tests/protocols/gmpv214/test_gmp_types.py
+++ tests/protocols/gmpv214/test_gmp_types.py
@@ -25,25 +25,25 @@ class GmpWithStatementTestMixin:
     def test_types(self):
         with self.gmp:
             # Test that the values are equal
-            self.assertEquals(
+            self.assertEqual(
                 self.gmp.types.AlertEvent.TASK_RUN_STATUS_CHANGED.value,
                 "Task run status changed",
             )
-            self.assertEquals(
+            self.assertEqual(
                 self.gmp.types.PermissionSubjectType.USER.value, "user"
             )
-            self.assertEquals(
+            self.assertEqual(
                 self.gmp.types.HostsOrdering.RANDOM.value, "random"
             )
 
             # Test usability of from_string
-            self.assertEquals(
+            self.assertEqual(
                 self.gmp.types.HostsOrdering.from_string("reverse"),
                 self.gmp.types.HostsOrdering.REVERSE,
             )
 
             # Test, that the Enum class types are equal
-            self.assertEquals(self.gmp.types.HostsOrdering, HostsOrdering)
+            self.assertEqual(self.gmp.types.HostsOrdering, HostsOrdering)
 
 
 class Gmpv214WithStatementTestCase(GmpWithStatementTestMixin, Gmpv214TestCase):
