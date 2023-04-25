6e602c46aa0a4f515e709a7eaf0a8b909bb750a9
Jaspar S <jaspar.loechte@greenbone.net>
2023-04-25 15:15:57 +0200

    Add: Readd get_nvts and get_nvt original calls (#1008)

--- tests/protocols/gmpv208/entities/secinfo/test_get_nvt.py
+++ tests/protocols/gmpv208/entities/secinfo/test_get_nvt.py
@@ -42,3 +42,23 @@ class GmpGetNvtTestMixin:
 
         with self.assertRaises(RequiredArgument):
             self.gmp.get_nvt(nvt_id=None)
+
+    def test_get_extended_nvt_with_nvt_oid(self):
+        self.gmp.get_nvt(extended=True, nvt_id="nvt_oid")
+
+        self.connection.send.has_been_called_with(
+            (
+                '<get_nvts nvt_oid="nvt_oid" details="1" '
+                'preferences="1" preference_count="1"/>'
+            )
+        )
+
+    def test_get_extended_nvt_missing_nvt_oid(self):
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_nvt(extended=True, nvt_id=None)
+
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_nvt(extended=True, nvt_id="")
+
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_nvt("", extended=True)
