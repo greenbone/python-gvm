9175aad80ea790b4e70ea33fb35afe5114006efa
Björn Ricks <bjoern.ricks@greenbone.net>
2024-03-06 15:43:28 +0100

    Add: Add a decorator for marking code as deprecated

--- gvm/utils.py
+++ gvm/utils.py
@@ -9,7 +9,8 @@ import base64
 import logging
 import re
 import warnings
-from typing import Any, List, Union
+from functools import wraps
+from typing import Any, Callable, List, Optional, Type, Union
 
 from gvm.xml import XmlError, parse_xml
 
@@ -26,6 +27,79 @@ class TypesDict(dict):
 
 def deprecation(message: str):
     warnings.warn(message, DeprecationWarning, stacklevel=2)
+
+
+def deprecated(
+    _func_or_cls: Union[str, Callable, Type, None] = None,
+    *,
+    since: Optional[str] = None,
+    reason: Optional[str] = None,
+):
+    """
+    A decorator to mark functions, classes and methods as deprecated
+
+    Args:
+        since: An optional version since the referenced item is deprecated.
+        reason: An optional reason why the references item is deprecated.
+
+    Examples:
+        .. code-block:: python
+
+            from pontos.helper import deprecated
+
+            @deprecated
+            def my_function(*args, **kwargs):
+                ...
+
+            @deprecated("The function is obsolete. Please use my_func instead.")
+            def my_function(*args, **kwargs):
+                ...
+
+            @deprecated(
+                since="1.2.3",
+                reason="The function is obsolete. Please use my_func instead."
+            )
+            def my_function(*args, **kwargs):
+                ...
+
+            @deprecated(reason="The class will be removed in version 3.4.5")
+            class Foo:
+                ...
+
+            class Foo:
+                @deprecated(since="2.3.4")
+                def bar(self, *args, **kwargs):
+                    ...
+    """
+    if isinstance(_func_or_cls, str):
+        reason = _func_or_cls
+        _func_or_cls = None
+
+    def decorator_repeat(func_or_cls):
+        module = func_or_cls.__module__
+        name = func_or_cls.__name__
+
+        if module == "__main__":
+            msg = f"{name} is deprecated."
+        else:
+            msg = f"{module}.{name} is deprecated."
+
+        if since:
+            msg += f" It is deprecated since version {since}."
+        if reason:
+            msg += f" {reason}"
+
+        @wraps(func_or_cls)
+        def wrapper(*args, **kwargs):
+            warnings.warn(msg, category=DeprecationWarning, stacklevel=3)
+            return func_or_cls(*args, **kwargs)
+
+        return wrapper
+
+    if _func_or_cls is None:
+        return decorator_repeat
+    else:
+        return decorator_repeat(_func_or_cls)
 
 
 def check_command_status(xml: str) -> bool:
