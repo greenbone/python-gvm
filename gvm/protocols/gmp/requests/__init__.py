97f4b9fd086f05009040e28a77e6fdb11d1693d7
Björn Ricks <bjoern.ricks@greenbone.net>
2024-03-12 13:25:54 +0100

    Add: Implement Aggregates command

--- gvm/protocols/gmp/requests/__init__.py
+++ gvm/protocols/gmp/requests/__init__.py
@@ -2,16 +2,22 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from ._aggregates import Aggregates, AggregateStatistic, SortOrder
 from ._auth import Authentication
+from ._entity_type import EntityType
 from ._port_list import PortList, PortRangeType
 from ._resource_names import ResourceNames, ResourceType
 from ._version import Version
 
 __all__ = (
+    "AggregateStatistic",
+    "Aggregates",
     "Authentication",
     "PortList",
     "PortRangeType",
     "Version",
     "ResourceNames",
     "ResourceType",
+    "SortOrder",
+    "EntityType",
 )
