8244497a0a5714a43785a8bb57ccabce5e54638b
Björn Ricks <bjoern.ricks@greenbone.net>
2024-03-21 16:05:08 +0100

    Change: Return a string from to_base64 function
    The function is used to convert a string input into a base64 string for
    a xml command. Therefore it is best to return a string instead of bytes.

--- tests/utils/test_to_base64.py
+++ tests/utils/test_to_base64.py
@@ -11,10 +11,10 @@ from gvm.utils import to_base64
 class TestToBase64(unittest.TestCase):
     def test_to_base64(self):
         foo64 = to_base64("foo")
-        self.assertEqual(b"Zm9v", foo64)
+        self.assertEqual("Zm9v", foo64)
 
         bar64 = to_base64("bar")
-        self.assertEqual(b"YmFy", bar64)
+        self.assertEqual("YmFy", bar64)
 
 
 if __name__ == "__main__":
