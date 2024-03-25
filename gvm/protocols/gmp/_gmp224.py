843d5d882d25ecdd12f3bec9657c3a3c6825f106
Björn Ricks <bjoern.ricks@greenbone.net>
2024-03-25 11:33:59 +0100

    Add scanner methods for new protocol implementation

--- gvm/protocols/gmp/_gmp224.py
+++ gvm/protocols/gmp/_gmp224.py
@@ -18,6 +18,8 @@ from .requests import (
     PortList,
     PortRangeType,
     ScanConfigs,
+    Scanners,
+    ScannerType,
     SortOrder,
     SystemReports,
     TrashCan,
@@ -663,4 +665,145 @@ class GMPv224(GvmProtocol[T]):
             ScanConfigs.modify_scan_config_set_family_selection(
                 config_id, families, auto_add_new_families=auto_add_new_families
             )
+        )
+
+    def create_scanner(
+        self,
+        name: str,
+        host: str,
+        port: Union[str, int],
+        scanner_type: ScannerType,
+        credential_id: str,
+        *,
+        ca_pub: Optional[str] = None,
+        comment: Optional[str] = None,
+    ) -> T:
+        """Create a new scanner
+
+        Args:
+            name: Name of the new scanner
+            host: Hostname or IP address of the scanner
+            port: Port of the scanner
+            scanner_type: Type of the scanner
+            credential_id: UUID of client certificate credential for the
+                scanner
+            ca_pub: Certificate of CA to verify scanner certificate
+            comment: Comment for the scanner
+        """
+        return self._send_and_transform_command(
+            Scanners.create_scanner(
+                name,
+                host,
+                port,
+                scanner_type,
+                credential_id,
+                ca_pub=ca_pub,
+                comment=comment,
+            )
+        )
+
+    def modify_scanner(
+        self,
+        scanner_id: EntityID,
+        *,
+        name: Optional[str] = None,
+        host: Optional[str] = None,
+        port: Optional[int] = None,
+        scanner_type: Optional[ScannerType] = None,
+        credential_id: Optional[EntityID] = None,
+        ca_pub: Optional[str] = None,
+        comment: Optional[str] = None,
+    ) -> T:
+        """Modify an existing scanner
+
+        Args:
+            scanner_id: UUID of the scanner to modify
+            name: New name of the scanner
+            host: New hostname or IP address of the scanner
+            port: New port of the scanner
+            scanner_type: New type of the scanner
+            credential_id: New UUID of client certificate credential for the
+                scanner
+            ca_pub: New certificate of CA to verify scanner certificate
+            comment: New comment for the scanner
+        """
+        return self._send_and_transform_command(
+            Scanners.modify_scanner(
+                scanner_id,
+                name=name,
+                host=host,
+                port=port,
+                scanner_type=scanner_type,
+                credential_id=credential_id,
+                ca_pub=ca_pub,
+                comment=comment,
+            )
+        )
+
+    def get_scanners(
+        self,
+        *,
+        filter_string: Optional[str] = None,
+        filter_id: Optional[EntityID] = None,
+        trash: Optional[bool] = None,
+        details: Optional[bool] = None,
+    ) -> T:
+        """Request a list of scanners
+
+        Args:
+            filter_string: Filter term to use for the query
+            filter_id: UUID of an existing filter to use for the query
+            trash: Whether to get the trashcan scanners instead
+            details: Whether to include extra details like tasks using this
+                scanner
+        """
+        return self._send_and_transform_command(
+            Scanners.get_scanners(
+                filter_string=filter_string,
+                filter_id=filter_id,
+                trash=trash,
+                details=details,
+            )
+        )
+
+    def get_scanner(self, scanner_id: EntityID) -> T:
+        """Request a single scanner
+
+        Args:
+            scanner_id: UUID of an existing scanner
+        """
+        return self._send_and_transform_command(
+            Scanners.get_scanner(scanner_id)
+        )
+
+    def verify_scanner(self, scanner_id: EntityID) -> T:
+        """Verify an existing scanner
+
+        Args:
+            scanner_id: UUID of an existing scanner
+        """
+        return self._send_and_transform_command(
+            Scanners.verify_scanner(scanner_id)
+        )
+
+    def clone_scanner(self, scanner_id: EntityID) -> T:
+        """Clone an existing scanner
+
+        Args:
+            scanner_id: UUID of an existing scanner
+        """
+        return self._send_and_transform_command(
+            Scanners.clone_scanner(scanner_id)
+        )
+
+    def delete_scanner(
+        self, scanner_id: EntityID, ultimate: Optional[bool] = False
+    ) -> T:
+        """Delete an existing scanner
+
+        Args:
+            scanner_id: UUID of an existing scanner
+        """
+        return self._send_and_transform_command(
+            Scanners.delete_scanner(scanner_id, ultimate=ultimate)
         )
