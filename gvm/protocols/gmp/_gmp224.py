617ccf585e209440a214673262577182cd944322
Björn Ricks <bjoern.ricks@greenbone.net>
2024-05-22 13:18:11 +0200

    Implement operating systems for new protocol

--- gvm/protocols/gmp/_gmp224.py
+++ gvm/protocols/gmp/_gmp224.py
@@ -30,6 +30,7 @@ from .requests import (
     Hosts,
     HostsOrdering,
     Notes,
+    OperatingSystems,
     Overrides,
     PortList,
     PortRangeType,
@@ -2364,4 +2365,70 @@ class GMPv224(GvmProtocol[T]):
         """
         return self._send_and_transform_command(
             Hosts.modify_host(host_id, comment=comment)
+        )
+
+    def delete_operating_system(
+        self,
+        operating_system_id: EntityID,
+    ) -> T:
+        """Deletes an existing operating system
+
+        Args:
+            operating_system_id: UUID of the single operating_system to delete.
+        """
+        return self._send_and_transform_command(
+            OperatingSystems.delete_operating_system(operating_system_id)
+        )
+
+    def get_operating_systems(
+        self,
+        *,
+        filter_string: Optional[str] = None,
+        filter_id: Optional[EntityID] = None,
+        details: Optional[bool] = None,
+    ) -> T:
+        """Request a list of operating systems
+
+        Args:
+            filter_string: Filter term to use for the query
+            filter_id: UUID of an existing filter to use for the query
+            details: Whether to include additional information (e.g. tags)
+        """
+        return self._send_and_transform_command(
+            OperatingSystems.get_operating_systems(
+                filter_string=filter_string,
+                filter_id=filter_id,
+                details=details,
+            )
+        )
+
+    def get_operating_system(
+        self, operating_system_id: EntityID, *, details: Optional[bool] = None
+    ) -> T:
+        """Request a single operating system
+
+        Args:
+            operating_system_id: UUID of an existing operating_system
+            details: Whether to include additional information (e.g. tags)
+        """
+        return self._send_and_transform_command(
+            OperatingSystems.get_operating_system(
+                operating_system_id, details=details
+            )
+        )
+
+    def modify_operating_system(
+        self, operating_system_id: EntityID, *, comment: Optional[str] = None
+    ) -> T:
+        """Modifies an existing operating system.
+
+        Args:
+            operating_system_id: UUID of the operating_system to be modified.
+            comment: Comment for the operating_system. Not passing a comment
+                arguments clears the comment for this operating system.
+        """
+        return self._send_and_transform_command(
+            OperatingSystems.modify_operating_system(
+                operating_system_id, comment=comment
+            )
         )
