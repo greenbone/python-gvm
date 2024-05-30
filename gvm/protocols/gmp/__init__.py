b6afd5e3a3b1d65b8492e706e3d97273c0df1942
Björn Ricks <bjoern.ricks@greenbone.net>
2024-05-30 15:52:33 +0200

    Export GMPv224 and GMPv225 from gvm.protocols.gmp
    The module `gvm.protocols.gmp` should be the main entry point when using
    python-gvm for GMP.

--- gvm/protocols/gmp/__init__.py
+++ gvm/protocols/gmp/__init__.py
@@ -3,10 +3,14 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from ._gmp import GMP
+from ._gmp224 import GMPv224
+from ._gmp225 import GMPv225
 
 Gmp = GMP  # for backwards compatibility
 
 __all__ = (
     "GMP",
     "Gmp",
+    "GMPv224",
+    "GMPv225",
 )
