190e0e823fa4cc0f4154c569a3c8238fe74480ea
Björn Ricks <bjoern.ricks@greenbone.net>
2024-05-31 10:19:26 +0200

    Remove code for the old GMP protocol implementations
    This also removes all code providing GMP 20.8 and 21.4.

--- gvm/protocols/gmpv208/entities/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2021-2024 Greenbone AG
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
--- gvm/protocols/gmpv225/entities/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2023-2024 Greenbone AG
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
