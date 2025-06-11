76ee16685587672d19f6c9e6d28e7d349a25890f
Michael Wiegand <michael.wiegand@greenbone.net>
2025-06-11 13:24:14 +0200

    Tests: Add tests for current and future GMP versions
    Add tests to make sure the detected protocol version and instance is
    correct for GMP 22.7.
    
    Also add tests for the expected next protocol version (`22.8`) and a
    possible far future version (`22.20`), which should adequately fall back
    to the latest current version.

--- tests/protocols/gmp/test_context_manager.py
+++ tests/protocols/gmp/test_context_manager.py
@@ -11,6 +11,7 @@ from gvm.protocols.gmp import Gmp
 from gvm.protocols.gmp._gmp224 import GMPv224
 from gvm.protocols.gmp._gmp225 import GMPv225
 from gvm.protocols.gmp._gmp226 import GMPv226
+from gvm.protocols.gmp._gmp227 import GMPv227
 from tests.protocols import GmpTestCase
 
 
@@ -132,6 +133,49 @@ class GmpContextManagerTestCase(GmpTestCase):
         with self.gmp as gmp:
             self.assertEqual(gmp.get_protocol_version(), (22, 6))
             self.assertIsInstance(gmp, GMPv226)
+
+    def test_select_gmpv227(self):
+        self.connection.read.return_value(
+            b'<get_version_response status="200" status_text="OK">'
+            b"<version>22.07</version>"
+            b"</get_version_response>"
+        )
+
+        with self.gmp as gmp:
+            self.assertEqual(gmp.get_protocol_version(), (22, 7))
+            self.assertIsInstance(gmp, GMPv227)
+
+        self.connection.read.return_value(
+            b'<get_version_response status="200" status_text="OK">'
+            b"<version>22.7</version>"
+            b"</get_version_response>"
+        )
+
+        with self.gmp as gmp:
+            self.assertEqual(gmp.get_protocol_version(), (22, 7))
+            self.assertIsInstance(gmp, GMPv227)
+
+    def test_next_version_fallback(self):
+        self.connection.read.return_value(
+            b'<get_version_response status="200" status_text="OK">'
+            b"<version>22.8</version>"
+            b"</get_version_response>"
+        )
+
+        with self.gmp as gmp:
+            self.assertEqual(gmp.get_protocol_version(), (22, 7))
+            self.assertIsInstance(gmp, GMPv227)
+
+    def test_newer_version_fallback(self):
+        self.connection.read.return_value(
+            b'<get_version_response status="200" status_text="OK">'
+            b"<version>22.20</version>"
+            b"</get_version_response>"
+        )
+
+        with self.gmp as gmp:
+            self.assertEqual(gmp.get_protocol_version(), (22, 7))
+            self.assertIsInstance(gmp, GMPv227)
 
     def test_unknown_protocol(self):
         self.connection.read.return_value(
