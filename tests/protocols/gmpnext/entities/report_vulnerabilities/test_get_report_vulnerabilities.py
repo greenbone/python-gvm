274731b1446ccb3f2a9e2ea2712caec6c70cb977
ozgen <ozgn45@gmail.com>
2026-05-08 10:51:37 +0200

    change: rename vuns to vulnerabilities for clarity

--- tests/protocols/gmpnext/entities/report_vulnerabilities/test_get_report_vulnerabilities.py
+++ tests/protocols/gmpnext/entities/report_vulnerabilities/test_get_report_vulnerabilities.py
@@ -6,49 +6,55 @@
 from gvm.errors import RequiredArgument
 
 
-class GmpGetReportVulnsTestMixin:
+class GmpGetReportVulnerabilitiesTestMixin:
     def test_get_report_vulns_without_id(self):
         with self.assertRaises(RequiredArgument):
-            self.gmp.get_report_vulns(None)
+            self.gmp.get_report_vulnerabilities(None)
 
         with self.assertRaises(RequiredArgument):
-            self.gmp.get_report_vulns("")
+            self.gmp.get_report_vulnerabilities("")
 
     def test_get_report_vulns_with_filter_string(self):
-        self.gmp.get_report_vulns(report_id="r1", filter_string="name=foo")
+        self.gmp.get_report_vulnerabilities(
+            report_id="r1", filter_string="name=foo"
+        )
 
         self.connection.send.has_been_called_with(
             b'<get_report_vulns report_id="r1" filter="name=foo" details="1"/>'
         )
 
     def test_get_report_vulns_with_filter_id(self):
-        self.gmp.get_report_vulns(report_id="r1", filter_id="f1")
+        self.gmp.get_report_vulnerabilities(report_id="r1", filter_id="f1")
 
         self.connection.send.has_been_called_with(
             b'<get_report_vulns report_id="r1" filt_id="f1" details="1"/>'
         )
 
     def test_get_report_vulns_with_ignore_pagination(self):
-        self.gmp.get_report_vulns(report_id="r1", ignore_pagination=True)
+        self.gmp.get_report_vulnerabilities(
+            report_id="r1", ignore_pagination=True
+        )
 
         self.connection.send.has_been_called_with(
             b'<get_report_vulns report_id="r1" ignore_pagination="1" details="1"/>'
         )
 
-        self.gmp.get_report_vulns(report_id="r1", ignore_pagination=False)
+        self.gmp.get_report_vulnerabilities(
+            report_id="r1", ignore_pagination=False
+        )
 
         self.connection.send.has_been_called_with(
             b'<get_report_vulns report_id="r1" ignore_pagination="0" details="1"/>'
         )
 
     def test_get_report_vulns_with_details(self):
-        self.gmp.get_report_vulns(report_id="r1", details=True)
+        self.gmp.get_report_vulnerabilities(report_id="r1", details=True)
 
         self.connection.send.has_been_called_with(
             b'<get_report_vulns report_id="r1" details="1"/>'
         )
 
-        self.gmp.get_report_vulns(report_id="r1", details=False)
+        self.gmp.get_report_vulnerabilities(report_id="r1", details=False)
 
         self.connection.send.has_been_called_with(
             b'<get_report_vulns report_id="r1" details="0"/>'
