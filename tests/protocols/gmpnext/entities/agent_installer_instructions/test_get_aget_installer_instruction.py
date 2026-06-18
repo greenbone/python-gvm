a20c51bdabcc25e4e5c2691aae245f2244b24b22
ozgen <ozgn45@gmail.com>
2026-06-18 13:01:16 +0200

    add: support origin URL for agent installer instructions
    Add the required `origin_url` argument to the agent installer instruction request and public GMP method.
    
    Include the origin URL in the generated GMP command and update the related tests.

--- tests/protocols/gmpnext/entities/agent_installer_instructions/test_get_aget_installer_instruction.py
+++ tests/protocols/gmpnext/entities/agent_installer_instructions/test_get_aget_installer_instruction.py
@@ -14,20 +14,42 @@ class GmpGetAgentInstallerInstructionTestMixin:
         self.gmp.get_agent_installer_instruction(
             scanner_id="scanner1",
             language_type=AgentInstallerInstructionLanguageType.EN,
+            origin_url="https://example.com",
         )
 
         self.connection.send.has_been_called_with(
-            b'<get_agent_installer_instruction scanner_id="scanner1" language="en"/>'
+            b"<get_agent_installer_instruction"
+            b' scanner_id="scanner1"'
+            b' language="en"'
+            b' origin_url="https://example.com"/>'
         )
 
     def test_get_agent_installer_instruction_de(self):
         self.gmp.get_agent_installer_instruction(
             scanner_id="scanner1",
             language_type=AgentInstallerInstructionLanguageType.DE,
+            origin_url="https://example.com",
         )
 
         self.connection.send.has_been_called_with(
-            b'<get_agent_installer_instruction scanner_id="scanner1" language="de"/>'
+            b"<get_agent_installer_instruction"
+            b' scanner_id="scanner1"'
+            b' language="de"'
+            b' origin_url="https://example.com"/>'
+        )
+
+    def test_get_agent_installer_instruction_escapes_origin_url(self):
+        self.gmp.get_agent_installer_instruction(
+            scanner_id="scanner1",
+            language_type=AgentInstallerInstructionLanguageType.EN,
+            origin_url="https://example.com/path?foo=bar&value=test",
+        )
+
+        self.connection.send.has_been_called_with(
+            b"<get_agent_installer_instruction"
+            b' scanner_id="scanner1"'
+            b' language="en"'
+            b' origin_url="https://example.com/path?foo=bar&amp;value=test"/>'
         )
 
     def test_get_agent_installer_instruction_without_scanner_id(self):
@@ -35,12 +57,14 @@ class GmpGetAgentInstallerInstructionTestMixin:
             self.gmp.get_agent_installer_instruction(
                 scanner_id=None,
                 language_type=AgentInstallerInstructionLanguageType.EN,
+                origin_url="https://example.com",
             )
 
         with self.assertRaises(RequiredArgument):
             self.gmp.get_agent_installer_instruction(
                 scanner_id="",
                 language_type=AgentInstallerInstructionLanguageType.EN,
+                origin_url="https://example.com",
             )
 
     def test_get_agent_installer_instruction_without_language_type(self):
@@ -48,10 +72,27 @@ class GmpGetAgentInstallerInstructionTestMixin:
             self.gmp.get_agent_installer_instruction(
                 scanner_id="scanner1",
                 language_type=None,
+                origin_url="https://example.com",
             )
 
         with self.assertRaises(RequiredArgument):
             self.gmp.get_agent_installer_instruction(
                 scanner_id="scanner1",
                 language_type="",
+                origin_url="https://example.com",
+            )
+
+    def test_get_agent_installer_instruction_without_origin_url(self):
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_agent_installer_instruction(
+                scanner_id="scanner1",
+                language_type=AgentInstallerInstructionLanguageType.EN,
+                origin_url=None,
+            )
+
+        with self.assertRaises(RequiredArgument):
+            self.gmp.get_agent_installer_instruction(
+                scanner_id="scanner1",
+                language_type=AgentInstallerInstructionLanguageType.EN,
+                origin_url="",
             )
